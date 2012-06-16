d2fd33aa62d77f73bef7d40040a3809f5a07f8c5
Luiz Fernando Ribeiro <luizfar@gmail.com>
2012-06-16 10:00:37 -0400

    Initial basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashmeet::Application.config.secret_token = '3a6347d5b2d57ae8ffa611c59b8b772fedea92607ea14e8050c94f47f26ca64bcb873ccfa958a66bb119e7dd478fbebeff4b1f035ebab472c5fc3a74f833e210'
