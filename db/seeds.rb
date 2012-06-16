b49985acf8cefa3a6359e32d59101c314ca18738
Luiz Fernando Ribeiro <luizfar@gmail.com>
2012-06-16 14:12:17 -0400

    capitalizing categories names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-categories = ['drink', 'eat', 'rock', 'explore', 'chill']
+categories = ['Drink', 'Eat', 'Rock', 'Explore', 'Chill']
 categories.each { |category| Category.create(category: category)}
 
 # This file should contain all the record creation needed to seed the database with its default values.
